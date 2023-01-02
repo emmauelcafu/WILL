93061a4c626fe92b2382d92fea05305bec03f3a3
emmanuelcafu <emmauelcafu@gmail.com>
2023-01-02 00:37:19 -0500

    ejercicio realizado

--- 06-07-08.js
+++ 06-07-08.js
@@ -11,6 +11,11 @@ function crearClasePersona() {
       // Inicializar las propiedades de la persona con los valores recibidos como argumento
 
       // Tu código aca:
+      this.nombre = nombre;
+      this.edad = edad;
+      this.hobbies = hobbies;
+      this.amigos = amigos;
+
 
     }
 
@@ -20,6 +25,9 @@ function crearClasePersona() {
       // No debe retornar nada.
 
       // Tu código aca:
+      let amigo = {nombre, edad};
+      this.amigos.push(amigo);
+
 
     }
 
@@ -28,6 +36,8 @@ function crearClasePersona() {
       // No debe retornar nada.
 
       // Tu código aca:
+      this.hobbies.push(hobby);
+     
 
     }
     getFriends() {
@@ -38,6 +48,9 @@ function crearClasePersona() {
       // persona.getFriends() debería devolver ['martin', 'toni']
 
       // Tu código aca:
+      const transformarObjetoAmigoANombre = ((amigo) => amigo.nombre);
+            const indexed = this.amigos.map(transformarObjetoAmigoANombre);
+            return indexed
 
     }
 
@@ -47,6 +60,7 @@ function crearClasePersona() {
       // persona.getHobbies() debe devolver ['correr', 'dormir', 'nadar']
 
       // Tu código aca:
+      return this.hobbies;
 
     }
 
@@ -66,6 +80,7 @@ function crearClasePersona() {
       // persona.getPromedioEdad() debería devolver 29 ya que (33 + 25) / 2 = 29
 
       // Tu código aca:
+      return this.amigos.map(amigo => amigo.edad).reduce((a, b) => a + b) / this.amigos.length
 
     }
   };
